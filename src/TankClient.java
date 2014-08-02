addbf809632bee923093a8ffc1480ce0ba155141
syang <kugelfish@gmail.com>
2014-08-02 20:29:27 +0200

    repaint tanks

--- src/TankClient.java
+++ src/TankClient.java
@@ -6,11 +6,18 @@ import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame {
 
+  int x = 50;
+  int y = 50;
+
   public static void main (String[] args) {
 
     TankClient tankClient = new TankClient ();
-
     tankClient.lauchFrame ();
+
+    for (int i = 0; i < 100; i++) {
+      System.out.println ("Main print----- " + i);
+    }
+
   }
 
   public void lauchFrame () {
@@ -30,14 +37,36 @@ public class TankClient extends Frame {
     this.setResizable (false);
     this.setBackground (Color.GREEN);
     setVisible (true);
+    new Thread (new PaintThread ()).start ();
   }
 
   @Override
   public void paint (Graphics g) {
     Color color = g.getColor ();
     g.setColor (Color.RED);
-    g.fillOval (50, 50, 30, 30);
+    g.fillOval (x, y, 30, 30);
     g.setColor (color);
 
+    y += 5;
   }
+
+  private class PaintThread implements Runnable {
+
+    @Override
+    public void run () {
+
+      while (true) {
+        System.out.println ("PaintThread print-----y: " + y);
+        repaint ();
+        try {
+          Thread.sleep (100);
+        } catch (InterruptedException e) {
+          e.printStackTrace ();
+        }
+
+      }
+    }
+
+  }
+
 }
