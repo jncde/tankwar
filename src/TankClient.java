985ede214a1d4995492fd0bab22ec235da8b5d53
syang <kugelfish@gmail.com>
2014-08-02 19:30:23 +0200

    no resizing; closing event

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,16 +1,31 @@
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame {
 
   public static void main (String[] args) {
 
     TankClient tankClient = new TankClient ();
+
     tankClient.lauchFrame ();
   }
 
   public void lauchFrame () {
     this.setLocation (400, 300);
     this.setSize (800, 600);
+
+    this.addWindowListener (new WindowAdapter () {
+
+      @Override
+      public void windowClosing (WindowEvent e) {
+        super.windowClosing (e);
+        System.exit (0);
+      }
+
+    });
+    this.setResizable (false);
     setVisible (true);
   }
+
 }
